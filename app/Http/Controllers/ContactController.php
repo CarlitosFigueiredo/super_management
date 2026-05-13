86ccc16632a702052cbf50f7d773ad4a11ef66a9
Carlos Yuri Figueiredo <92981178+CarlitosFigueiredo@users.noreply.github.com>
2026-05-13 14:08:34 -0300

    form get contact create

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -18,9 +18,9 @@ class ContactController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        dd($request->all());
     }
 
     /**
